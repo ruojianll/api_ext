c526a064e6613da5d82a821d0cf79153f1da5297
ruojian <ruojianll@gmail.com>
2017-12-08 12:06:35 +0800

    ext alert,console.log

--- api-ext.js
+++ api-ext.js
@@ -51,8 +51,54 @@
     function apiExt(aim) {
 
     }
+
+    //在这里扩展全局对象
+    function globalExt(aim){
+        var alert_ = aim.alert;
+        aim.alert = function(){
+            var str = '';
+            for(var i in arguments)
+            {
+                var p = arguments[i];
+                switch (typeof p) {
+                    case 'string':
+                        str += p;
+                        break;
+                    case 'object':
+                        str += JSON.stringify(p);
+                }
+                if(i < arguments.length - 1){
+                    str += '\r\n------\r\n';
+                }
+            }
+            alert_.call(aim,str);
+        }
+
+        var console_log_ = aim.console.log;
+        aim.console.log = function(){
+            var str = '';
+            for(var i in arguments)
+            {
+                var p = arguments[i];
+                switch (typeof p) {
+                    case 'string':
+                        str += p;
+                        break;
+                    case 'object':
+                        str += JSON.stringify(p);
+                }
+                if(i < arguments.length - 1){
+                    str += '\r\n---\r\n';
+                }
+            }
+            console_log_.call(aim.console,str);
+        }
+    }
     //--------------------------------------------------------------
     function init() {
+
+        globalExt(global_)
+
         if (!$api || !(typeof $api === 'object')) {
             global_$api = {};
         }
