31a7283f13dba74ee86cb497db5f745bad95d0a1
ruojian <ruojianll@gmail.com>
2017-12-08 18:40:08 +0800

    parse UIMoudle

--- api-ext.js
+++ api-ext.js
@@ -151,6 +151,38 @@
     var $apiExts = [];
     var apiExts = [];
     global_.api_ext = {
+        parseUIMoudle : function(type){
+            filter = 'ui-moudle';
+            if(type && typeof type === 'string'){
+                filter += '[type="'+ type + '"]'
+            }
+
+            var eles = document.querySelectorAll(filter);
+
+            for(var i in eles){
+                var ele = eles.item(i);
+                type = ele.getAttribute('type');
+                type = type.toLowerCase();
+                switch (type) {
+                    case 'uiinput':
+                        var iUIInput = this._getUIModuleSingleton('UIInput');
+                        iUIInput.open({
+                            rect:$api.getRectOf(ele),
+                            fixedOn:api.frameName
+                        });
+                        break;
+                }
+
+            }
+        },
+
+        _getUIModuleSingleton : function(type){
+            if(!this._singletons[type]){
+                this._singletons[type] = api.require(type);
+            }
+            return this._singletons[type];
+        },
+        _singletons:{},
         registerCustomerApiExt : function(fn){
             apiExts.push(fn);
         },
